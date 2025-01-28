cc032447c0ba5700dba1877704029da8cd8a6498
Igrium <thesam54123@gmail.com>
2025-01-28 14:16:53 -0800

    implemented PID controller in physics hand

--- code/VRComponents/VRPhysicsHand.cs
+++ code/VRComponents/VRPhysicsHand.cs
@@ -13,35 +13,71 @@ public sealed class VRPhysicsHand : Component
 	[Property, RequireComponent]
 	public Rigidbody? Rigidbody { get; private set; }
 
+	[Property]
+	public float posKp { get; set; } = 4000f;
+
+	[Property]
+	public float posKd { get; set; } = 400f;
+	
+	[Property]
+	public float rotKp { get; set; } = 600000f;
+
+	[Property]
+	public float rotKd { get; set; } = 100000f;
+
+
+	public float rotMagnitudeClamp = 100000f;
+
+	[Property]
+	public bool UseRealForce { get; set; } = true;
+
+	private Vector3 prevRotDifference = Vector3.Zero;
+	private Vector3 prevPosDifference = Vector3.Zero;
+
 	public VRTrackedHand? TrackingHandComponent { get => TrackingHand?.GetComponent<VRTrackedHand>(); }
 
-	private PhysicsJoint? joint;
 
 	//protected override void OnUpdate()
 	//{
 	//	base.OnUpdate();
 	//	if ( Rigidbody.IsValid() && TrackingHand.IsValid() )
 	//	{
-	//		Rigidbody.SmoothMove( TrackingHand.WorldTransform, 0, 0 );
+	//		Rigidbody.SmoothMove( TrackingHand.WorldTransform, .0001f, Time.Delta );
 	//	}
+
 	//}
 
-	//protected override void OnStart()
-	//{
-	//	base.OnStart();
-	//	var trackingHand = TrackingHandComponent;
-	//	if ( Rigidbody.IsValid() && trackingHand.IsValid() )
-	//	{
-	//		joint = PhysicsJoint.CreateFixed( new PhysicsPoint( trackingHand.PhysicsParent ), new PhysicsPoint( Rigidbody.PhysicsBody ) );
-	//		Log.Info( joint.Point1.Body );
-	//	}
-	//}
-	protected override void OnUpdate()
+	protected override void OnFixedUpdate()
 	{
-		base.OnUpdate();
+		base.OnFixedUpdate();
 		if ( Rigidbody.IsValid() && TrackingHand.IsValid() )
 		{
-			Rigidbody.SmoothMove( TrackingHand.WorldTransform, .0001f, Time.Delta );
+			Vector3 force = PositionPD( GameObject.WorldPosition, TrackingHand.WorldPosition );
+			//force = force.ClampLength( 1000f );
+			Rigidbody.PhysicsBody.ApplyForce( force );
+
+			Vector3 torque = RotationPD( GameObject.WorldRotation, TrackingHand.WorldRotation );
+			//torque.ClampLength( rotMagnitudeClamp );
+			Rigidbody.PhysicsBody.ApplyTorque( torque );
 		}
+		
+	}
+
+	private Vector3 PositionPD(Vector3 currentPos, Vector3 targetPos)
+	{
+		Vector3 p = targetPos - currentPos;
+		Vector3 d = (p - prevPosDifference) / Time.Delta;
+		prevPosDifference = p;
+
+		return p * posKp + d * posKd;
+	}
+
+	private Vector3 RotationPD( Rotation currentRotation, Rotation targetRotation )
+	{
+		Rotation rot = targetRotation * currentRotation.Inverse;
+		Vector3 p = new Vector3( rot.x, rot.y, rot.z ) * rot.w * Time.Delta;
+		Vector3 d = (p - prevRotDifference) / Time.Delta;
+		prevRotDifference = p;
+		return p * rotKp + d * rotKd;
 	}
 }
