d5805621b097320b6674ebb624073a2c95bd7578
Igrium <thesam54123@gmail.com>
2025-01-29 19:33:51 -0800

    experimenting with capsule collider

---
+++ code/VRComponents/VRPlayerCollider.cs
@@ -0,0 +1,34 @@
+﻿namespace VRBase;
+
+public sealed class VRPlayerCollider : Component
+{
+	[Property, RequireComponent]
+	public CapsuleCollider? Collider { get; set; }
+
+	[Property]
+	public GameObject? HMD { get; set; }
+
+	public Vector3 LastValidWorldPos { get; private set; }
+
+	protected override void OnUpdate()
+	{
+		base.OnUpdate();
+		if (Collider != null && HMD != null)
+		{
+			// Move the collider
+			Vector3 targetPos = HMD.WorldPosition;
+			targetPos.z = Collider.WorldPosition.z;
+
+			Collider.WorldPosition = targetPos;
+
+			Collider.End = new Vector3(0, 0, targetPos.z);
+
+			if (!Collider.Touching.Any())
+			{
+				LastValidWorldPos = Collider.WorldPosition;
+			}
+			//Collider.g
+		}
+		
+	}
+}
