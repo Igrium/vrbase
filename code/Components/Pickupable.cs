b7d1ed9434783be37a913d2ed12b6d94890585ca
Da0neDatGotAway <43645756+Da0neDatGotAway@users.noreply.github.com>
2025-02-24 04:27:12 -0800

    added descriptions of classes and functions, and redid the implemetation to hopefully be easier for developers

--- code/Components/Pickupable.cs
+++ code/Components/Pickupable.cs
@@ -1,4 +1,5 @@
 using Sandbox;
+using Sandbox.Diagnostics;
 
 namespace VRBase;
 
@@ -10,14 +11,18 @@ public sealed class Pickupable : Component
 
 	public PIDController? pid;
 
+	private Logger log = new Logger("Pickupable: ");
+
 	protected override void OnUpdate()
 	{
 		if(pid.IsValid())
 		{
+			//if we have a PID, but no hands holding destroy our PID
 			if(handsHolding.Count == 0)
 			{
 				pid.Destroy();
 			}
+			//otherwise update our target PID position to the average of all hands holding
 			else
 			{
 				pid.targetPos = averagedPos();
@@ -26,22 +31,29 @@ public sealed class Pickupable : Component
 		}
 	}
 
+	//gets the averaged position of all hands currently holding this object, using their pickupData and position
 	private Vector3 averagedPos()
 	{
-		if(handsHolding.Count == 1)
-		{
-			return handsHolding[0].WorldPosition;
-		}
-
 		Vector3 outVect = Vector3.Zero;
 		foreach(Vrpickup hand in handsHolding)
 		{
-			outVect += hand.WorldPosition;
+			PickupData? data = hand.GetHeldObject();
+			
+			if(data != null && data.Value.Object == this)
+			{
+				outVect += hand.WorldPosition - data.Value.RelativePos;
+			}
+			else
+			{
+				log.Error("Our hand contained bad data, forcing drop");
+				drop(null);
+			}
 		}
 		outVect = outVect/handsHolding.Count;
 		return outVect;
 	}
 
+	//get the averaged rotation of all hands currently holding this object, using their pickupData and rotation
 	private Rotation averagedRot()
 	{
 		if(handsHolding.Count == 1)
@@ -51,11 +63,19 @@ public sealed class Pickupable : Component
 		return Rotation.Identity;
 	}
 
-	public Pickupable? pickup(Vrpickup hand)
+	private PIDController createPID(Vrpickup hand)
+	{
+		PIDController pid = this.AddComponent<PIDController>();
+		return pid;
+	}
+
+	//called whenever the player attempts to pick up an object 
+	//TODO: implement some interface with the hand animation tree to control pose
+	public PickupData? pickup(Vrpickup hand)
 	{
 		if(!pid.IsValid())
 		{
-			pid = this.AddComponent<PIDController>();
+			this.pid = createPID(hand);
 			if(rigidbody.IsValid())
 			{
 				pid.Rigidbody = rigidbody;
@@ -65,18 +85,25 @@ public sealed class Pickupable : Component
 		if(!handsHolding.Contains(hand))
 		{
 			handsHolding.Add(hand);
-			return this;
+			//this is temporary, make it actually use an algoritim to get these positions
+			return new PickupData(this, hand.WorldPosition, hand.WorldRotation);
 		}
 		return null;
 	}
 
+	//selected hand drops the object, all hands drop if null
 	public void drop(Vrpickup? hand)
 	{
 		if(!hand.IsValid())
 		{
+			foreach(Vrpickup h in handsHolding)
+			{
+				h.dropHeldObject();
+			}
 			handsHolding.Clear();
 			return;
 		}
+		hand.dropHeldObject();
 		handsHolding.Remove(hand);
 	}
 }
--- code/VRComponents/Vrpickup.cs
+++ code/VRComponents/Vrpickup.cs
@@ -21,14 +21,15 @@ public sealed class Vrpickup : Component
 
 	private Boolean grabIsPressed = false;
 
-	private Pickupable? heldObject;
+	private PickupData? heldObject;
 	private Logger log = new Logger("VRpickup: ");
 
 	protected override void OnUpdate()
 	{
-		//a visualizer for the sphere, except this method crashes s&bbox every time...
+		//a visualizer for the sphere, except this method was broken by s&box and now crashes every time...
 		//DebugOverlay.Sphere(new Sphere(getGrabZoneCenter(), grabZonePickupRadius));
 
+		//if we are in VR, s&box currently does not have any way good to specify input for controllers except to hardcode it. That's what this is
 		if(Game.IsRunningInVR)
 		{
 			float currentGripValue;
@@ -57,6 +58,7 @@ public sealed class Vrpickup : Component
 				}
 			}
 		}
+		//2D, debug purposes
 		else
 		{
 			if( Input.Pressed("use") )
@@ -70,36 +72,72 @@ public sealed class Vrpickup : Component
 		}
 	}
 
+	public PickupData? GetHeldObject()
+	{
+		return this.heldObject;
+	}
+
+	//updates our held object, making sure to drop our current held object
+	private void dropAndSetHeldObject(PickupData? pickup)
+	{
+		//store it in a variable. This is done to make all the functions related to drop call eachother to have a less chaotic mess
+		PickupData? oldHeldObject = heldObject;
+
+		
+		heldObject = pickup;
+		if(oldHeldObject != null && oldHeldObject.Value.Object.IsValid())
+		{
+			oldHeldObject.Value.Object.drop(this);
+		}
+	}
+
+	//get the center of our grab sphere in world cordinates
 	public Vector3 getGrabZoneCenter()
 	{
 		return this.WorldTransform.PointToWorld(grabZoneCenter);
 	}
 
-	public void setHeldObject(Pickupable? pickup)
+	//makes this hand drops any held object
+	public void dropHeldObject()
 	{
-		if(heldObject.IsValid())
-		{
-			heldObject.drop(this);
-		}
-		heldObject = pickup;
+		dropAndSetHeldObject(null);
 	}
 
+	//a function for when the "grab button" is pressed
 	public void onGrabPressed()
 	{
+		//find all pickupable objects in our pickupsphere, and try to pick them up
 		SceneTrace trace = Scene.Trace.Sphere(grabZonePickupRadius, getGrabZoneCenter(), getGrabZoneCenter());
 		foreach(SceneTraceResult result in trace.RunAll())
 		{
 			Pickupable pickup = result.Body.GetGameObject().GetComponent<Pickupable>();
 			if(pickup != null)
 			{
-				setHeldObject(pickup.pickup(this));
+				dropAndSetHeldObject(pickup.pickup(this));
 				break;
 			}
 		}
 	}
 
+	//a function for when our "grab button" is released
 	public void onGrabReleased()
 	{
-		setHeldObject(null);
+		dropHeldObject();
 	}
+}
+
+//A simple data structure that holds our objects origin, and our objects rotation, relative to the hand that is holding us to be applied every PID calculation, and to be rendered to the visual hand for asthetics
+public struct PickupData
+{
+	public Pickupable Object { get; }
+	public Vector3 RelativePos { get; }
+    public Rotation RelativeRot { get; }
+    public PickupData(Pickupable pickupObject, Vector3 relativePos, Rotation relativeRot)
+    {
+		Object = pickupObject;
+        RelativePos = relativePos;
+        RelativeRot = relativeRot;
+    }
+
+    public override string ToString() => $"({Object.GameObject.Name}, {RelativePos}, {RelativeRot})";
 }
